6bf48e8436ca71d04160b7c8a02e6bd2c07fdfa6
casiamasuku <casia.masuku@outlook.com>
2023-07-13 11:33:00 -0700

    advanced task update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints "and that piece of art is useful" - - Dora Korpar
- * followed by a new line, to standard error.
- * Return: 1 always
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-    fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-    return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
