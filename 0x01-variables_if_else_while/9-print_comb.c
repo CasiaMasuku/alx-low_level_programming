edd40555e7b1ab8a9e60bd2b44d9f21ff3527dd9
casiamasuku <casia.masuku@outlook.com>
2023-07-14 10:06:26 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,35 +1,25 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - represent Entry Point to the program
  *
  * Return: 0 to stop the program
  */
-void my_putchar(char c) {
-    write(1, &c, 1);
+int main(void)
+{
+unsigned char a = '0';
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(a);
+a++;
 }
-
-void print_combinations() {
-    int i = 0;
-    int j = 0;
-
-    while (i < 10) {
-        j = i + 1;
-        while (j < 10) {
-            my_putchar(i + '0');
-            my_putchar(',');
-            my_putchar(' ');
-            my_putchar(j + '0');
-            if (i != 8 || j != 9) {
-                my_putchar(',');
-                my_putchar(' ');
-            }
-            j++;
-        }
-        i++;
-    }
+a = '1';
+for (i = 0; i < 6; i++)
+{
+putchar('0' + a);
+a++;
 }
-
-int main() {
-    print_combinations();
-    return 0;
+putchar('\n');
+return (0);
 }
