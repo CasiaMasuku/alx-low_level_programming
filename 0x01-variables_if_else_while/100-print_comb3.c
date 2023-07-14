1c1e8619a3363722c864b41250fe9887940d167d
casiamasuku <casia.masuku@outlook.com>
2023-07-14 10:33:29 -0700

    advanced task1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point 
+ * Prints numbers 0 to 99
+ * Return: (0)
+ * */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int count = 0;
+
+	while (i <= 8)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (count != 44)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+			count++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
