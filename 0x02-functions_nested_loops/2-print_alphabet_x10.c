209043e314fffdb2d6e7453fa4fd27a3fdf333e0
casiamasuku <casia.masuku@outlook.com>
2023-07-18 07:21:57 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10-prints 10 times the alphabet, in lowercase
+ *
+ * return: void
+ */
+void print_alphabet_x10(void)
+{
+int x = 0;
+char alpha = 'z'
+for (x = 0; x <= 9; x++)
+putchar(alpha)
+}
