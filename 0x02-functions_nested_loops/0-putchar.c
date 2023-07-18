a23d4c34050857bf2fcc1eb9231cc0c2788bdfd8
casiamasuku <casia.masuku@outlook.com>
2023-07-18 05:03:55 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,15 @@
  * prints _putchar, followed by a new line.
  * return (0)
  */
-int main(void)
-{
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
 
-    return (0);
-}
+    int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+}    return (0);
