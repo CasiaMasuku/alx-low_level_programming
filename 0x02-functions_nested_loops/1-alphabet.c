2d832a652fd8b3f72dff82e0b242da7bcc21770c
casiamasuku <casia.masuku@outlook.com>
2023-07-18 02:50:39 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prints the alphabet, in lowercase, followed by a new line.
+ * no return
+ */
+void print_alphabet(void);
+{
+for (char alpha = 'a'; alpha <= 'z'; alpha++){
+        _putchar(alpha);
+    }
+    _putchar('\n');
+}
