24c6f38d47aa12016c08c413a209bef0ab26aea0
casiamasuku <casia.masuku@outlook.com>
2023-07-19 13:20:50 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,34 +8,37 @@
  *Return: 0
  */
 void print_to_98(int n)
-if (n > 98)
+{
+int x;
+    if (n > 98)
 {
 for (x = n; x >= 98; x--)
 {
 if (x == 98)
-printf(x)
+{
+printf("%d",x);
 }
 else
 {
-printf(x);
+printf("%d",x);
 _putchar(',');
 _putchar(' ');
 }
 }
-{
-else
 }
+else
+{
 for (x = n; x <= 98; x++)
 if (x == 98)
 {
-printf(x);
+printf("%d",x);
 }
 else
 {
-printf(x);
+printf("%d",x);
 _putchar(',');
 _putchar(' ');
-{
-_putchar('\n');
 }
+}
+_putchar('\n');
 }
