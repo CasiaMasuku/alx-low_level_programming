e6619d29383f9acab035161ed5f6a794022dbf35
casiamasuku <casia.masuku@outlook.com>
2023-07-19 14:23:21 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,11 +5,10 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-int i = 0;
-positive_or_negative(i);
-
+int x;
+x = 0;
+positive_or_negative(x);
 return (0);
 }
