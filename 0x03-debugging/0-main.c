60e00914a1d3cd190b63c126cccac3c31b3ffc05
casiamasuku <casia.masuku@outlook.com>
2023-07-19 14:08:35 -0700

    debugging task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+positive_or_negative(int i);
+
+return (0);
+}
