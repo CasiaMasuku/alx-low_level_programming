f98d794e7098f3d629b04c0de89979d58df0326f
casiamasuku <casia.masuku@outlook.com>
2023-07-19 08:38:05 -0700

    task10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*add-adds two integers and returns the result.
-*@x: is the first integer
-*@z: is the second integer
-*Returns: x + z
-*/
+ *add - adds two integers and returns the result
+ *@x: is the first integer
+ *@z: is the second integer
+ *Return: x + z
+ */
 int add(int x, int z)
 {
-return (int x + int z);
+return (x + z);
 }
