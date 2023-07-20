c82a9de322675f85588e32fa04f8056c0228624e
casiamasuku <casia.masuku@outlook.com>
2023-07-20 06:14:18 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,11 @@
 *_isdigit - verifies if a character is a digit
 *@c: tested character
 *
-*Return: returns 1 if it is uppercase , 0 if not
+*Return: returns 1 if uppercase , 0 else
 */
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <= 57))
+if (c >= 48 && c <= 57)
 return (1);
 
 return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 int _isupper(int);
 int _isdigit(int);
+
+#endif /* MAIN_H */
