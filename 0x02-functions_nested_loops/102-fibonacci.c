d1d44b5c5b34ce692d41b8e78ee73bd72657e05f
casiamasuku <casia.masuku@outlook.com>
2023-07-19 23:08:29 -0700

    task14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * return: 0
+ */
+int main(void)
+{
+int sum;
+unsigned long x1 = 0, x2 = 1, x3;
+for (sum = 0; sum < 50; sum++)
+{
+x3 = x1 + x2;
+printf("%lu", x3);
+x1 = x2;
+x2 = x3;
+if (sum == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
