fc6c53cd6cf0a8bfc48627c7e4739d48823cb691
casiamasuku <casia.masuku@outlook.com>
2023-07-20 17:51:29 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,11 +18,11 @@ if ((a % 3 == 0 && a % 5) != 0)
 {
 printf("Fizz");
 }
-else if ((a % 5 == 0 && a % 3) != 0) 
+else if ((a % 5 == 0 && a % 3) != 0)
 {
 printf("Buzz");
 }
-else if (a % 3 == 0 && a % 5 == 0) 
+else if (a % 3 == 0 && a % 5 == 0)
 {
 printf("FizzBuzz");
 }
