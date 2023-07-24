9e7d4b6f1a37f215688b4db0f8d6cd729813b406
casiamasuku <casia.masuku@outlook.com>
2023-07-24 04:27:43 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: is the string to be considered
+ *
+ * Return: string and number of characters
+ */
+void _puts(char *str)
+{
+_putchar(*str);
+_putchar('\n');
+}
