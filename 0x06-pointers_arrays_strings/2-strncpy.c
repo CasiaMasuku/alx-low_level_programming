f60101ba0fea0f187b5daa09e72690060763286b
casiamasuku <casia.masuku@outlook.com>
2023-07-27 03:37:29 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int a;
-for (a = 0; a < n && src[a] != '\\0'; a++)
+a = 0;
+while (a < n && src[a] != '\0')
+{
 dest[a] = src[a];
-for (; a < n; a++)
-dest[a] = '\\0';
+a++;
+}
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
 return (dest);
 }
