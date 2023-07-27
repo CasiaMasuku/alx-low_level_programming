7dcf42532e2807687bb6db209a8269def8b19d61
casiamasuku <casia.masuku@outlook.com>
2023-07-27 05:04:30 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,32 +8,16 @@
  */
 char *rot13(char *str)
 {
-int index1, index2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',
-'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z', 'a', 'b', 'c', 'd',
-e', 'f', 'g', 'h', 'i', 'j',
-'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v',
-'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-'T', 'U', 'V', 'W', 'X', 'Y',
-'Z', 'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'n', 'o', 'p', 'q',
-'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z', 'a', 'b', 'c',
-'d', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'l', 'm'};
-while (str[++index1])
+int i, j;
+char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; str[i] != '\0'; ++i)
 {
-for (index2 = 0; index2 < 52; index2++)
+for (j = 0; j < 52; ++j)
 {
-if (str[index1] == alphabet[index2])
+if (str[i] == arr1[j])
 {
-str[index1] = rot13key[index2];
+str[i] = arr2[j];
 break;
 }
 }
