9d609c5daeb894ae756d51c859bc174aeec5075b
casiamasuku <casia.masuku@outlook.com>
2023-07-31 14:50:42 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,17 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; (s[i] != c) && (s[i] != '\0'); i++);
-if (s[i] == c)
-return (s + i);
-else
-return (NULL);
+while (*s != '\0') /*Declaring WHILE*/
+{
+if (*s == c) /*if s ==  c*/
+{
+return (s); /*return s*/
+}
+++s;
+}
+if (*s == c)
+{
+return (s);
+}
+return (0);/* values null*/
 }
