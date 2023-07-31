5e0975cfee26ee36a2290ae69d250fdd82da6259
casiamasuku <casia.masuku@outlook.com>
2023-07-31 07:56:35 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,11 +17,3 @@ for (i = 0; i < n; i++)
 s[i] = b;
 return (s);
 }
-int main(void)
-{
-char str[10] = "Hello";
-printf("Before memset(): %s\n", str);
-_memset(str + 2, 'l', 3);
-printf("After memset(): %s\n", str);
-return (0);
-}
