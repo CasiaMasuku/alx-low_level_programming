b47bce9258c4fe8ea46507254fc3920e8e7bd9b0
casiamasuku <casia.masuku@outlook.com>
2023-07-31 12:26:16 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,7 +9,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-char *t;
-t = strstr(haystack, needle);
-return (t);
+while (*haystack)
+{
+char *Begin = haystack;
+char *pattern = needle;
+while (*haystack && *pattern && *haystack == *pattern)
+{
+haystack++;
+pattern++;
+}
+if (!*pattern)
+return (Begin);
+haystack = Begin + 1;
+}
+return (NULL);
 }
