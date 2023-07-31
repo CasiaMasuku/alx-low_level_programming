5338b8868077536ebd9f11c4f63b3bdfcd9ea85a
casiamasuku <casia.masuku@outlook.com>
2023-07-31 14:16:07 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,19 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int sum1, sum2;
-sum1 = 0;
-sum2 = 0;
-i = 0;
-while (i < size)
+int i = 0;
+int j = size - 1;
+int sum1 = 0;
+int sum2 = 0;
+while (i <= (size * size))
 {
-sum1 = sum1 + *(a + i *size + i);
-sum2 = sum2 + *(a + i *size + size - i - 1);
-i++;
+sum1 = sum1 + a[i];
+i = i + size + 1;
 }
-printf("%i, %i\n", sum1, sum2);
+while (j < (size * size - 1))
+{
+sum2 += a[j];
+j = j + size - 1;
+}
+printf("%d, %d\n", sum1, sum2);
 }
