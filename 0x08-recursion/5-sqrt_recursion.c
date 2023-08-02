d6563bb99896be2b42b07469e15a23f5b8c50e0a
casiamasuku <casia.masuku@outlook.com>
2023-08-02 11:21:43 -0700

    C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,21 @@
  */
 int _sqrt_recursion(int n)
 {
+return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
 if (n < 0)
 return (-1);
-if (n == 0 || n == 1)
-return (n);
-return (_sqrt_recursion(n / 2) * 2 == n ? _sqrt_recursion(n / 2) : -1);
+if ((i * i) > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
