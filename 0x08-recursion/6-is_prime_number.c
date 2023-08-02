12baa81db8f5a8250ef8a950d199b67d92a77001
casiamasuku <casia.masuku@outlook.com>
2023-08-02 13:29:11 -0700

    C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,17 +8,21 @@
  */
 int is_prime_number(int n)
 {
-int i;
 if (n <= 1)
-{
 return (0);
+return (actual_prime(n, n - 1));
 }
-for (i = 2; i < n; i++)
-{
-if (n % i == 0)
-{
+
+/**
+ * actual_prime - calculates if number is prime
+ * @n: integer to consider
+ * @i: iterator
+ *
+ * Return: 1 if n is prime
+ */
+int actual_prine( int n, int i)
+{if (i == 1)
+return (1)
+if (n % i == 0 && i > 0)
 return (0);
-}
-}
-return (1);
-}
+return (actual_prime(n, i -1))
