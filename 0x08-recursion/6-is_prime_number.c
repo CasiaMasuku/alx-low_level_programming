7017210df88b6947b3997e5eb831131d09495023
casiamasuku <casia.masuku@outlook.com>
2023-08-02 02:40:46 -0700

    C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,18 +8,12 @@
  */
 int is_prime_number(int n)
 {
-if (n <= 1)
-return (0);
-if (n <= 3)
-return (1);
-if (n % 2 == 0 || n % 3 == 0)
-return (0);
-int i = 5;
-while (i * i <= n)
+if ((!(n % 2) && n != 2) || n < 2)
 {
-if (n % i == 0 || n % (i + 2) == 0)
 return (0);
-i += 6;
 }
-return (1);
+else
+{
+return (divisors(3, n));
+}
 }
