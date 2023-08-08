fdf163ebaa8980021e1bbdc7cd36059df2edc1d4
casiamasuku <casia.masuku@outlook.com>
2023-08-08 03:58:36 -0700

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 /**
- * ch_tmp_grid - frees a 2 dimensional array.
+ * ch_free_grid - frees a 2 dimensional array.
  * @grid: multidimensional array of char.
  * @height: height of the array.
  *
  * Return: no return
  */
-void ch_tmp_grid(char **grid, unsigned int height)
+void ch_free_grid(char **grid, unsigned int height)
 {
 if (grid != NULL && height != 0)
 {
 for (; height > 0; height--)
-tmp(grid[height]);
-tmp(grid[height]);
-tmp(grid);
+free(grid[height]);
+free(grid[height]);
+free(grid);
 }
 }
 
@@ -27,17 +27,17 @@ tmp(grid);
  */
 char **strtow(char *str)
 {
-char **num;
+char **aout;
 unsigned int c, height, i, j, a1;
 if (str == NULL || *str == '\0')
 return (NULL);
 for (c = height = 0; str[c] != '\0'; c++)
 if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 height++;
-num = malloc((height + 1) * sizeof(char *));
-if (num == NULL || height == 0)
+aout = malloc((height + 1) * sizeof(char *));
+if (aout == NULL || height == 0)
 {
-tmp(num);
+free(aout);
 return (NULL);
 }
 for (i = a1 = 0; i < height; i++)
@@ -48,19 +48,19 @@ if (str[c] == ' ')
 a1++;
 if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 {
-num[i] = malloc((c - a1 + 2) * sizeof(char));
-if (num[i] == NULL)
+aout[i] = malloc((c - a1 + 2) * sizeof(char));
+if (aout[i] == NULL)
 {
-ch_tmp_grid(num, i);
+ch_free_grid(aout, i);
 return (NULL);
 }
 break;
 }
 }
 for (j = 0; a1 <= c; a1++, j++)
-num[i][j] = str[a1];
-num[i][j] = '\0';
+aout[i][j] = str[a1];
+aout[i][j] = '\0';
 }
-num[i] = NULL;
-return (num);
+aout[i] = NULL;
+return (aout);
 }
