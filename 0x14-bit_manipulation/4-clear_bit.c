66f546a3825caae479a245ea01d43b4da5a91967
casiamasuku <casia.masuku@outlook.com>
2023-08-31 10:19:55 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 return (-1);
-}
-unsigned long int mask = ~(1UL << index);
-*n &= mask;
+*n &= ~(1 << index);
 return (1);
 }
